4ac5f73792f5565f67dee7fab43e65e218389a01
Asilbek <asilbekkaromatov2@gmail.com>
2026-06-06 11:17:18 +0500

    defer(on viewport)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,16 @@
-import { Component, effect, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { Comments } from "./comments/comments";
+import { Article } from './article/article';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.html',
-  imports: [],
+  templateUrl: './app.html', 
+  imports: [Comments, Article],
 })
 export class App {
-  email = signal('');
-
-  constructor() {
-    effect(() => {
-      const keyword = this.email();
-      if (keyword.length >= 3) {
-        console.log(`Searching for: ${keyword}` );
-      }
-    });
-  }
-
-  onEmailChange(event: Event) {
-    const input = event.target as HTMLInputElement;
-    this.email.set(input.value);
-  }
+ 
 }
+
+// @defer - Bu asosiy blok. Component yuklanayotganda, bu kod blokini kechiktirish uchun ishlatiladi. Bu foydalanuvchi tajribasini yaxshilashga yordam beradi, chunki asosiy tarkib tezroq yuklanadi va foydalanuvchi interaktiv bo'lishi mumkin.
+// @placeholder - Bu kod blokini yuklashdan oldin ko'rsatiladigan o'rinbosar. Bu foydalanuvchilarga kontent yuklanayotganini ko'rsatadi va ularni kutishga undaydi.
+// @loading - Bu kod blokini yuklash jarayonida ko'rsatiladigan yuklanish indikatorini belgilaydi. Bu foydalanuvchilarga kontent yuklanayotganini ko'rsatadi va ularni kutishga undaydi.
