70c6722d68826afa656d33288a993f6f6a8a4b15
Asilbek <asilbekkaromatov2@gmail.com>
2026-06-08 23:29:08 +0500

    angular material added

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
 import { IProduct } from '../interfaces/product';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +11,8 @@ export class ProductService {
   private http = inject(HttpClient);
   private apiUrl = environment.apiUrl;
 
-  getProducts():Observable<IProduct[]> {
+  getProducts(): Observable<IProduct[]> {
     return this.http.get<IProduct[]>(`${this.apiUrl}/products`);
   }
+
 }
